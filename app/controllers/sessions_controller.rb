a3831502196e705e69cb4a42eca505f96bdf16f5
Noah Berman <noah@noahberman.org>
2016-12-05 10:59:01 -0700

    working basic oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,22 @@
 class SessionsController < ApplicationController
+
 				def create
-					@response = Faraday.post("https://github.com/login/oauth/access_token?client_id=github_key&client_secret=github_secret&code=#{params["code"]}")
+					@response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV['GITHUB_CLIENT_KEY']}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}&code=#{params["code"]}")
+					
+					token = @response.body.split(/\W+/)[1]
+
+					oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+
+					auth = JSON.parse(oauth_response.body)
+
+					user					= User.find_or_create_by(uid: auth["uid"])
+					user.username = auth["login"]
+					user.uid			= auth["id"]
+					user.token		= token
+					user.save
+
+					session[:user_id] = user.id
+
+					redirect_to dashboard_index_path
 				end
-				
-				token = @response.body.split(/\w+/)[1]
-
-				oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
-
-				auth = JSON.parse(oauth_response.body)
-
-				private
-
-				github_secret = ENV["GITHUB_CLIENT_SECRET"]
-				github_key = ENV["GITHUB_CLIENT_KEY"]
 end
-
